5461bcf6569f66fa7459dd940d72e3af45fbfe9a
itsmejhansi06-ship-it <itsmejhansi06@gmail.com>
2026-03-07 16:34:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ progress = {}
 buffers = {}
 lock = threading.Lock()
 
+
 # ---------- SSL CONTROL CHANNEL ----------
 def control_server():
 
@@ -32,7 +33,6 @@ def control_server():
         client, addr = tcp.accept()
 
         try:
-
             secure = context.wrap_socket(client, server_side=True)
 
             data = secure.recv(1024).decode()
@@ -43,9 +43,7 @@ def control_server():
             with lock:
                 last = progress.get(filename, -1)
 
-            secure.send(json.dumps({
-                "resume_from": last
-            }).encode())
+            secure.send(json.dumps({"resume_from": last}).encode())
 
             secure.close()
 
@@ -65,46 +63,45 @@ def handle_packet(data, addr):
     try:
 
         header_len = int.from_bytes(data[:2], "big")
-        header = json.loads(data[2:2+header_len].decode())
-        payload = data[2+header_len:]
+        header = json.loads(data[2:2 + header_len].decode())
+        payload = data[2 + header_len:]
 
         filename = header["filename"]
         recv_path = os.path.join("recv_test_files", os.path.basename(filename))
-
-        if filename not in buffers:
-            buffers[filename] = {}
 
         # ---------- DATA ----------
         if header["type"] == "DATA":
 
             seq = header["seq"]
 
-            buffers[filename][seq] = payload
-
             with lock:
+
+                if filename not in buffers:
+                    buffers[filename] = {}
+
+                buffers[filename][seq] = payload
+
                 last = progress.get(filename, -1)
 
-            # write packets in correct order
-            while True:
+                while True:
 
-                next_seq = last + 1
+                    next_seq = last + 1
 
-                if next_seq not in buffers[filename]:
-                    break
+                    if next_seq not in buffers[filename]:
+                        break
 
-                if next_seq == 0:
-                    open(recv_path, "wb").close()
+                    if next_seq == 0:
+                        open(recv_path, "wb").close()
 
-                with open(recv_path, "ab") as f:
-                    f.write(buffers[filename].pop(next_seq))
+                    with open(recv_path, "ab") as f:
+                        f.write(buffers[filename].pop(next_seq))
 
-                last = next_seq
-
-                with lock:
+                    last = next_seq
                     progress[filename] = last
 
-                print(f"{addr} -> packet {last}")
+                    print(f"{addr} -> packet {last}")
 
+            # ---------- SEND ACK ----------
             ack = json.dumps({
                 "type": "ACK",
                 "seq": seq
@@ -149,6 +146,7 @@ def handle_packet(data, addr):
 
 # ---------- START CONTROL SERVER ----------
 threading.Thread(target=control_server, daemon=True).start()
+
 
 # ---------- UDP RECEIVE LOOP ----------
 while True:
